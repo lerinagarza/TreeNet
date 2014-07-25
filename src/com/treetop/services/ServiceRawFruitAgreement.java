bd93f79a054732116e7a4f17b48e246eae1073e2
John Hager <john.hagler@treetop.com>
2014-07-25 09:56:16 -0700

    test service commit comments

--- src/com/treetop/services/ServiceRawFruitAgreement.java
+++ src/com/treetop/services/ServiceRawFruitAgreement.java
@@ -28,7 +28,7 @@ public class ServiceRawFruitAgreement {
             inqRFA.setWriteUpNumber("123");
             getAgreement(inqRFA);
 
-            // test code
+            // test codes
 
         } catch (Exception e)         {
             fail(e.toString());
